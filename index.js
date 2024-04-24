6ebd4a02b502e08fa1ca2f7ded0b4c177558f040
Gabriellllla <marcugabriella02@gmail.com>
2024-04-24 19:19:45 +0300

    try1

--- index.js
+++ index.js
@@ -16,8 +16,8 @@
 // const app = initializeApp(firebaseApp);
 
 
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, push } from "firebase/database";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app/"
