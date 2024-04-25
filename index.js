cff02b8f722cc1a5190c37c51fc6088b60084ae8
Gabriellllla <marcugabriella02@gmail.com>
2024-04-25 16:44:27 +0300

    try7

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-// import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from "firebase/analytics";
+ import { initializeApp } from 'firebase/app';
+ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-// const firebaseApp = initializeApp({
-//   apiKey: "AIzaSyDY4OQCbazOZQ9EIFZR5iY1tfV5yQ2IJ4g",
-//   authDomain: "bee-connected-2a01a.firebaseapp.com",
-//   databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "bee-connected-2a01a",
-//   storageBucket: "bee-connected-2a01a.appspot.com",
-//   messagingSenderId: "1037546953098",
-//   appId: "1:1037546953098:web:66810ebfba86ff36111073",
-//   measurementId: "G-00SQH47GGE"
-// });
+const firebaseConfig = {
+  apiKey: "AIzaSyDY4OQCbazOZQ9EIFZR5iY1tfV5yQ2IJ4g",
+  authDomain: "bee-connected-2a01a.firebaseapp.com",
+  databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bee-connected-2a01a",
+  storageBucket: "bee-connected-2a01a.appspot.com",
+  messagingSenderId: "1037546953098",
+  appId: "1:1037546953098:web:66810ebfba86ff36111073",
+  measurementId: "G-00SQH47GGE"
+};
 
-// const analytics = getAnalytics(app);
-// const app = initializeApp(firebaseApp);
+firebase.initializeApp(firebaseConfig);
 
+// Obțineți referința către serviciul de autentificare
+const auth = firebase.auth();
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
 
 const appSettings = {
