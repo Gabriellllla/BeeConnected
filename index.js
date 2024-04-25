17b1500f388d1ee882e4239ba76cfd7672bb033a
Gabriellllla <marcugabriella02@gmail.com>
2024-04-25 16:53:42 +0300

    10

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
 //import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+//import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
