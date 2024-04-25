2259907b423e4e45f1baa36a70ffa049e7a944c1
Gabriellllla <marcugabriella02@gmail.com>
2024-04-25 17:06:27 +0300

    try 12

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
- import { initializeApp } from 'firebase/app';
- import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { initializeApp } from 'firebase/app';
+//  import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDY4OQCbazOZQ9EIFZR5iY1tfV5yQ2IJ4g",
-  authDomain: "bee-connected-2a01a.firebaseapp.com",
-  databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "bee-connected-2a01a",
-  storageBucket: "bee-connected-2a01a.appspot.com",
-  messagingSenderId: "1037546953098",
-  appId: "1:1037546953098:web:66810ebfba86ff36111073",
-  measurementId: "G-00SQH47GGE"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDY4OQCbazOZQ9EIFZR5iY1tfV5yQ2IJ4g",
+//   authDomain: "bee-connected-2a01a.firebaseapp.com",
+//   databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "bee-connected-2a01a",
+//   storageBucket: "bee-connected-2a01a.appspot.com",
+//   messagingSenderId: "1037546953098",
+//   appId: "1:1037546953098:web:66810ebfba86ff36111073",
+//   measurementId: "G-00SQH47GGE"
+// };
+// const app = firebase.initializeApp(firebaseConfig);
+// // Obțineți referința către serviciul de autentificare
+// const auth = firebase.auth();
 
-const app = firebase.initializeApp(firebaseConfig);
-
-// Obțineți referința către serviciul de autentificare
-const auth = firebase.auth();
-
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-//import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+//import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 //import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
 
 const appSettings = {
   databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app/"
 }
-
-//const app = initializeApp(appSettings);
+const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const utilizatoriInDB = ref(database, "utilizatori");
-
 const numeInputEl = document.getElementById("nume");
 const prenumeInputEl = document.getElementById("prenume");
 const emailInputEl = document.getElementById("email");
 const parolaInputEl = document.getElementById("parola");
 const registerButtonEl = document.getElementById("register-button");
-
 registerButtonEl.addEventListener("click", function(){
     const nume = numeInputEl.value;
     const prenume = prenumeInputEl.value;
     const email = emailInputEl.value;
     const parola = parolaInputEl.value;
-
     // Înregistrare utilizator cu adresa de email și parola
     createUserWithEmailAndPassword(app.auth(), email, parola)
         .then((userCredential) => {
@@ -52,7 +47,6 @@ registerButtonEl.addEventListener("click", function(){
                 prenume: prenume,
                 email: email
             };
-
             // Salvare în baza de date
             push(utilizatoriInDB, userData)
                 .then(() => {
@@ -67,17 +61,11 @@ registerButtonEl.addEventListener("click", function(){
             console.error("Eroare la înregistrare în Firebase: ", error);
         });
 });
-
-
 // const inputFieldEl = document.getElementById("input-field");
 // const addButtonEl =  document.getElementById("add-button");
-
 // addButtonEl.addEventListener("click", function(){
 //   let inputValue = inputFieldEl.value;
-
 //   push(utilizatoriInDB, inputValue);
-
 //   console.log(`${inputValue} added to database`);
   
 // });
-
