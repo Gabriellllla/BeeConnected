7233386778425379414e1e704e0440b0a4555609
Gabriellllla <marcugabriella02@gmail.com>
2024-04-25 17:35:24 +0300

    try17

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// import { initializeApp } from 'firebase/app';
-//  import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDY4OQCbazOZQ9EIFZR5iY1tfV5yQ2IJ4g",
-//   authDomain: "bee-connected-2a01a.firebaseapp.com",
-//   databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "bee-connected-2a01a",
-//   storageBucket: "bee-connected-2a01a.appspot.com",
-//   messagingSenderId: "1037546953098",
-//   appId: "1:1037546953098:web:66810ebfba86ff36111073",
-//   measurementId: "G-00SQH47GGE"
-// };
-// const app = firebase.initializeApp(firebaseConfig);
-// // Obțineți referința către serviciul de autentificare
-// const auth = firebase.auth();
+const firebaseConfig = {
+  apiKey: "AIzaSyDY4OQCbazOZQ9EIFZR5iY1tfV5yQ2IJ4g",
+  authDomain: "bee-connected-2a01a.firebaseapp.com",
+  databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bee-connected-2a01a",
+  storageBucket: "bee-connected-2a01a.appspot.com",
+  messagingSenderId: "1037546953098",
+  appId: "1:1037546953098:web:66810ebfba86ff36111073",
+  measurementId: "G-00SQH47GGE"
+};
+const app = firebase.initializeApp(firebaseConfig);
+// Obțineți referința către serviciul de autentificare
+const auth = firebase.auth();
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+//import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+//import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 //import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 //import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
@@ -25,7 +25,7 @@ import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.
 const appSettings = {
   databaseURL: "https://bee-connected-2a01a-default-rtdb.europe-west1.firebasedatabase.app/"
 }
-const app = initializeApp(appSettings);
+//const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const utilizatoriInDB = ref(database, "utilizatori");
 const numeInputEl = document.getElementById("nume");
@@ -39,7 +39,7 @@ registerButtonEl.addEventListener("click", function(){
     const email = emailInputEl.value;
     const parola = parolaInputEl.value;
     // Înregistrare utilizator cu adresa de email și parola
-    createUserWithEmailAndPassword( app = initializeApp(appSettings), email, parola)
+    createUserWithEmailAndPassword(app.auth(), email, parola)
         .then((userCredential) => {
             // Salvare date suplimentare în baza de date
             const userData = {
