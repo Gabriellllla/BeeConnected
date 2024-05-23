0691af1bc745b78732438a17e7d17a358d9ba98b
Gabriellllla <marcugabriella02@gmail.com>
2024-05-23 21:19:28 +0300

    cvvv

--- logout.js
+++ logout.js
@@ -3,11 +3,16 @@ document.getElementById("logout-button").addEventListener("click", function(even
     // Opriți comportamentul implicit al legăturii
     event.preventDefault();
 
-    // Afiseaza dialogul de confirmare
-    var confirmLogout = confirm("Ești sigur că vrei să ieși?");
-    
-    // Dacă utilizatorul apasă OK în dialogul de confirmare, atunci redirecționează-l către pagina de login
-    if (confirmLogout) {
-        window.location.href = "index.html"; // Schimbă "login.html" cu pagina de login corectă
-    }
+    // Afișează dialogul
+    document.getElementById("dialog-overlay").style.display = "block";
+});
+
+// Ascunde dialogul când utilizatorul apasă butonul "Anulează"
+document.getElementById("cancel-button").addEventListener("click", function() {
+    document.getElementById("dialog-overlay").style.display = "none";
+});
+
+// Redirecționează utilizatorul către pagina de login când apasă butonul "Da"
+document.getElementById("confirm-button").addEventListener("click", function() {
+    window.location.href = "index.html"; // Schimbă "login.html" cu pagina de login corectă
 });
