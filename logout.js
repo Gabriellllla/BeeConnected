b6f098590908575dbca19ac2386f054bbf871f40
Gabriellllla <marcugabriella02@gmail.com>
2024-05-23 21:12:00 +0300

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,16 +1,13 @@
+// Afișează dialogul de confirmare când utilizatorul apasă butonul de delogare
 document.getElementById("logout-button").addEventListener("click", function(event) {
-    event.preventDefault(); // Previne comportamentul implicit al link-ului
+    // Opriți comportamentul implicit al legăturii
+    event.preventDefault();
 
-    // Afișează dialogul personalizat
-    document.getElementById("dialog-overlay").style.display = "block";
-
-    // Dacă utilizatorul apasă butonul "Da"
-    document.getElementById("confirm-button").addEventListener("click", function() {
-        window.location.href = "index.html"; // Redirecționează către pagina de logare
-    });
-
-    // Dacă utilizatorul apasă butonul "Anulează" sau închide dialogul
-    document.getElementById("cancel-button").addEventListener("click", function() {
-        document.getElementById("dialog-overlay").style.display = "none"; // Ascunde dialogul
-    });
+    // Afiseaza dialogul de confirmare
+    var confirmLogout = confirm("Ești sigur că vrei să ieși?");
+    
+    // Dacă utilizatorul apasă OK în dialogul de confirmare, atunci redirecționează-l către pagina de login
+    if (confirmLogout) {
+        window.location.href = "index.html"; // Schimbă "login.html" cu pagina de login corectă
+    }
 });
