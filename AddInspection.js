1a7d3584136d84aa9c1439b64702accc05aa7887
Gabriellllla <marcugabriella02@gmail.com>
2024-06-15 15:45:57 +0300

    Update AddInspection.js

--- AddInspection.js
+++ AddInspection.js
@@ -39,7 +39,7 @@ document.getElementById("add-inspection-form").addEventListener("submit", async 
     const Are_boli = document.getElementById("Are boli").checked;
     const Tipul_bolii = Are_boli ? document.getElementById("Tipul bolii").value : null;
     const Mâncare = document.getElementById("Mâncare").checked;
-    const TipM = Mâncare ? document.getElementById("Tipul mâncării").value : null;
+    const Tipul_mâncării = Mâncare ? document.getElementById("Tipul mâncării").value : null;
 
     const inspectionData = {
         date: Data_inspecției,
@@ -48,7 +48,7 @@ document.getElementById("add-inspection-form").addEventListener("submit", async 
         Are_boli: Are_boli,
         Tipul_bolii: Tipul_bolii,
         Mâncare: Mâncare,
-        TipM: TipM
+        Tipul_mâncării: Tipul_mâncării
     };
 
     onAuthStateChanged(auth, async (user) => {
